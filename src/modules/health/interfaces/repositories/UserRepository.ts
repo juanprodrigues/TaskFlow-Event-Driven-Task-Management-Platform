0353c64c1d71aaf9d48192afb65d83b06f59f2a7
Juan Rodriguez <juanprodrigues.33@gmail.com>
2026-07-11 19:51:46 -0300

    feat: entity user

---
+++ src/modules/health/interfaces/repositories/UserRepository.ts
@@ -0,0 +1,17 @@
+import { User } from "../../domain/entities/User";
+
+export interface UserRepository {
+
+    create(user: User): Promise<User>;
+
+    findById(id: string): Promise<User | null>;
+
+    findByEmail(email: string): Promise<User | null>;
+
+    findAll(): Promise<User[]>;
+
+    update(user: User): Promise<User>;
+
+    delete(id: string): Promise<void>;
+
+}
